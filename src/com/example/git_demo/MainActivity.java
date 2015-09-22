64a42e485fb24a678e7590a8e4abcca812542396
guohanqi <935686768@qq.com>
2015-09-23 00:29:42 +0800

    改了 东西    删除menu

--- src/com/example/git_demo/MainActivity.java
+++ src/com/example/git_demo/MainActivity.java
@@ -17,18 +17,17 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+		
+		
+		
+		System.out.println("这是第二次提交的");
+		System.out.println("这是第二次提交的,,,,,,,真的是第二次");
+		System.out.println("这是第二次提交的,,,,,,,真的是第二次,为什么eclipse不会自动编译");
+		System.out.println("这是第三次提交的,,,,,,,检查第二次为什么没有远程提交成功");
+		
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
